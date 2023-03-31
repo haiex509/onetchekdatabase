41dfe79ad56c8dbc940d291eed83452773842c80
jsbeaudry <jeansauvenel.beaudry@gmail.com>
2023-03-31 16:36:19 -0400

    first commit

--- src/api/holder/controllers/holder.js
+++ src/api/holder/controllers/holder.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
- * holder controller
+ * holder controllerdasadasdasd
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::holder.holder');
+module.exports = createCoreController("api::holder.holder");
