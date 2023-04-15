f0f388c7242cd6974403fba796d846bcf5e4cfb7
jsbeaudry <jeansauvenel.beaudry@gmail.com>
2023-04-15 11:02:09 -0400

    Added possiblity to block address

--- src/api/trade/controllers/trade.js
+++ src/api/trade/controllers/trade.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  * trade controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::trade.trade');
+module.exports = createCoreController("api::trade.trade");
